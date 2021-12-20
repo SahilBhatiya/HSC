adb1828da91fa26d29f002c7680b45285f99d3f7
Sahil Bhatiya <sahilbhatya999@outlook.com>
2021-12-20 22:34:14 +0530

    Add project files.

---
+++ HSC/Controllers/NavbarController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HSC.Controllers
+{
+    public class NavbarController : Controller
+    {
+        public IActionResult _Dashboard()
+        {
+            return View();
+        }
+    }
+}
