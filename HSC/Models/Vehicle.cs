753cc5d1b81f8c026c56e7248a1236174518f89a
Sahil Bhatiya <sahilbhatya999@outlook.com>
2021-12-22 00:25:38 +0530

    Change Naming

--- HSC/Models/Vehicle.cs
+++ HSC/Models/Vehicle.cs
@@ -1,10 +1,18 @@
-﻿namespace HSC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HSC.Models
 {
     public class Vehicle
     {
         public string Id { get; set; }
+
+        [Display(Name = "Number")]
         public string Number { get; set; }
+
+        [Display(Name = "Price / KM")]
         public int kmPrice { get; set; }
+
+        [Display(Name = "Owner")]
         public string owner { get; set; }
 
         public Vehicle()
